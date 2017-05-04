56b7d4c71e961a0097e7edb98a9325d5e913820d
Marlon Cordova <marloncordova@outlook.com>
2017-05-03 21:31:10 -0500

    Ultimo avance

--- AppApoderado/app/src/main/java/pe/edu/upc/appapoderado/network/NanaApi.java
+++ AppApoderado/app/src/main/java/pe/edu/upc/appapoderado/network/NanaApi.java
@@ -7,11 +7,11 @@ import pe.edu.upc.appapoderado.models.Usuario;
  */
 
 public class NanaApi {
-
-    public static String LOGIN_URL = "http://nanamia-001-site1.etempurl.com/api/loginapoderado";
-    public static String USER_REGISTER_URL = "http://nanamia-001-site1.etempurl.com/api/usuarioapoderado";
-    public static String NANA_URL = "http://nanamia-001-site1.etempurl.com/api/nana";
-    public static String SOLICITUD_URL = "http://nanamia-001-site1.etempurl.com/api/solicitud";
+     //http://nanamia-001-site1.etempurl.com
+    public static String LOGIN_URL = "http://localhost:9284/api/loginapoderado";
+    public static String USER_REGISTER_URL = "http://localhost:9284/api/usuarioapoderado";
+    public static String NANA_URL = "http://localhost:9284/api/nana";
+    public static String SOLICITUD_URL = "http://localhost:9284/api/solicitud";
 
     private Usuario currentUser;
 
